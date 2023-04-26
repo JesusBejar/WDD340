8e12f9e0af6d52ecc07954a77902a31a4f0b24e6
Jesus Bejar <israelbejarii@gmail.com>
2023-04-25 19:00:14 -0600

    added htdocs to 340 folder

---
+++ htdocs/phpmotors/template.php
@@ -0,0 +1,16 @@
+<header id="page_header">
+  <a href="/" title="Go to the subway.com home page">
+  <img src="/images/subway-logo.jpeg" alt="subway.com logo">
+  </a>
+  <div id="tools"> ... tools (e.g. search) go here ... </div>
+</header>
+<nav id="page_nav">
+  <ul>
+  <li><a href="/">Home</a><li>
+  <li><a href="/sandwiches">Classic</a><li>
+  <li><a href="/ingredients">Sports</a><li>
+  <li><a href="/catering">SUV</a><li>
+  <li><a href="/nutrition">Trucks</a><li>
+  <li><a href="/locations">Used</a><li>
+  </ul>
+</nav>
