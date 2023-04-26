3cf78d325e48fe541c2fa04da31b480052776d7c
Jesus Bejar <israelbejarii@gmail.com>
2023-04-25 19:55:55 -0600

    created template

---
+++ htdocs/phpmotors/template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Motors | Template</title>
+</head>
+<header id="header">
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?> 
+</header>
+<nav id="nav">
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/nav.php'; ?> 
+</nav>
+<body>
+    
+</body>
+<footer id="footer">
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?> 
+</footer>
+</html>
