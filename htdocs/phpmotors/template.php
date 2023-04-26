adea8767e6f1c41081412b279f29110d248de326
Jesus Bejar <israelbejarii@gmail.com>
2023-04-25 19:09:45 -0600

    added nav bar

--- htdocs/phpmotors/template.php
+++ htdocs/phpmotors/template.php
@@ -1,16 +1,11 @@
-<header id="page_header">
-  <a href="/" title="Go to the subway.com home page">
-  <img src="/images/subway-logo.jpeg" alt="subway.com logo">
-  </a>
-  <div id="tools"> ... tools (e.g. search) go here ... </div>
-</header>
+
 <nav id="page_nav">
   <ul>
-  <li><a href="/">Home</a><li>
-  <li><a href="/sandwiches">Classic</a><li>
-  <li><a href="/ingredients">Sports</a><li>
-  <li><a href="/catering">SUV</a><li>
-  <li><a href="/nutrition">Trucks</a><li>
-  <li><a href="/locations">Used</a><li>
+  <li><a href=".../index.php">Home</a><li>
+  <li><a href="#">Classic</a><li>
+  <li><a href="#">Sports</a><li>
+  <li><a href="#">SUV</a><li>
+  <li><a href="#">Trucks</a><li>
+  <li><a href="#">Used</a><li>
   </ul>
 </nav>
