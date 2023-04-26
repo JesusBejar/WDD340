b008174f904a1c5d5436381e0d89519d563d3770
Jesus Bejar <israelbejarii@gmail.com>
2023-04-25 19:22:31 -0600

    developed header and footer

--- htdocs/phpmotors/template.php
+++ htdocs/phpmotors/template.php
@@ -1,5 +1,10 @@
-
-<nav id="page_nav">
+<header id="header">
+  <a href="/" title="Go to the subway.com home page">
+  <img src=".../images/site/logo.png" alt="php motors logo">
+  </a>
+  <div id="my-account"> My Account </div>
+</header>
+<nav id="nav">
   <ul>
   <li><a href=".../index.php">Home</a><li>
   <li><a href="#">Classic</a><li>
@@ -9,3 +14,9 @@
   <li><a href="#">Used</a><li>
   </ul>
 </nav>
+<footer>
+    <h1>PHP Motors. All rights reserved</h1>
+        <p class="sub-footer">All images used are believed to be in Fair Use. Please notify if any are not and they will be removed</p>
+        <!-- last modified -->
+        <p id="last-modified"><span>Last modified : April 25 2023</span></p>
+</footer>
