da97778f4c0c1af3902125bf7f458fb600d7a10b
Jesus Bejar <israelbejarii@gmail.com>
2023-04-25 19:24:30 -0600

    separated header, nav & footer into separate files

---
+++ htdocs/phpmotors/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer>
+    <h1>PHP Motors. All rights reserved</h1>
+        <p class="sub-footer">All images used are believed to be in Fair Use. Please notify if any are not and they will be removed</p>
+        <!-- last modified -->
+        <p id="last-modified"><span>Last modified : April 25 2023</span></p>
+</footer>
---
+++ htdocs/phpmotors/header.php
@@ -0,0 +1,7 @@
+<!-- Header -->
+<header id="header">
+  <a href="/" title="Go to the subway.com home page">
+  <img src=".../images/site/logo.png" alt="php motors logo">
+  </a>
+  <div id="my-account"> My Account </div>
+</header>
