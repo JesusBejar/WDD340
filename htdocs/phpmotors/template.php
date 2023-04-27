f077bd31323cf1b629e0e15d2720f84c29248b6a
Jesus Bejar <israelbejarii@gmail.com>
2023-04-27 10:41:11 -0600

    developed template html and made index css file

--- htdocs/phpmotors/template.php
+++ htdocs/phpmotors/template.php
@@ -7,15 +7,20 @@
     <title>PHP Motors | Template</title>
 </head>
 <header id="header">
+    <!-- logo img wrapped with home link -->
+    <a href="template.php"><img src="/images/site/logo.png" alt="php motors logo"></a>
+    <!-- header php -->
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/header.php'; ?> 
 </header>
 <nav id="nav">
+    <!-- nav php -->
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/nav.php'; ?> 
 </nav>
 <body>
-    
+    <h1>CONTENT GOES HERE</h1>
 </body>
 <footer id="footer">
+    <!-- footer php -->
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/footer.php'; ?> 
 </footer>
 </html>
