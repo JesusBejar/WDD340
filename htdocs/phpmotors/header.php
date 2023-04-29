e9088541271397d254fae1669f34bc932f902567
Jesus Bejar <israelbejarii@gmail.com>
2023-04-28 19:04:33 -0600

    changed file location

--- htdocs/phpmotors/header.php
+++
@@ -1,7 +0,0 @@
-<!-- Header -->
-<header id="header">
-  <a href="/" title="Go to the subway.com home page">
-  <img src=".../images/site/logo.png" alt="php motors logo">
-  </a>
-  <div id="my-account"> My Account </div>
-</header>
