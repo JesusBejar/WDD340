aac5439b241f8d01e0dca16e1246162aadca631b
Jesus Bejar <israelbejarii@gmail.com>
2023-04-29 14:50:00 -0600

    finished making template!

--- phpmotors/template.php
+++ phpmotors/template.php
@@ -8,15 +8,15 @@
     <link rel="stylesheet" href="css/templateBase.css" type="text/css">
 </head>
 <header>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/header.php'; ?> 
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD340/phpmotors/header.php'; ?> 
 </header>
 <nav>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/nav.php'; ?> 
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD340/phpmotors/nav.php'; ?> 
 </nav>
 <body>
     <h1>Content Title Here</h1>
 </body>
 <footer>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/footer.php'; ?> 
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD340/phpmotors/footer.php'; ?> 
 </footer>
 </html>
