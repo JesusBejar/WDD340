a801a6ae405082b5cc3de844fb28ebefc43e263f
Jesus Bejar <israelbejarii@gmail.com>
2023-04-29 14:35:01 -0600

    changed file location to wdd340 folder

---
+++ phpmotors/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<p>PHP Motors. All rights reserved</p>
+<p class="sub-footer">All images used are believed to be in Fair Use. Please notify if any are not and they will be removed</p>
+<!-- last modified -->
+<p id="last-modified"><span>Last modified : April 25 2023</span></p>
---
+++ phpmotors/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Motors | Home</title>
+    <link rel="stylesheet" href="css/homeBase.css" type="text/css">
+</head>
+<header>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/header.php'; ?> 
+</header>
+<nav>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/nav.php'; ?> 
+</nav>
+<body>
+    <h1>Content Title Here</h1>
+</body>
+<footer>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/footer.php'; ?> 
+</footer>
+</html>
