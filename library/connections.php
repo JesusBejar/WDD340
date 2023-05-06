e4e9bc42ccf1a43eafe89fbca63ed607614e9d54
Jesus Bejar <israelbejarii@gmail.com>
2023-05-06 11:00:06 -0600

    added variables and assigned them

--- library/connections.php
+++ library/connections.php
@@ -1,2 +1,8 @@
 <?php
 // proxy connect to phpmotors data base
+
+$server = 'localhost';
+$dbname= 'phpmotors';
+$username = 'iClient';
+$password = ')lhxiT40B4l[93P*';
+$dsn = "mysql:host=$server;dbname=$dbname";
