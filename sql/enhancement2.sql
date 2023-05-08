5b697b55eb0ffe5dd5bbaf115a8a96726dd2ee48
Jesus Bejar <israelbejarii@gmail.com>
2023-05-08 08:55:10 -0600

    made queries 4 & 5

--- sql/enhancement2.sql
+++ sql/enhancement2.sql
@@ -4,12 +4,25 @@ INSERT INTO clients (clientEmail, clientFirstname, clientLastname, clientPasswor
 VALUES ("tony@starknet.com", "Tony", "Stark", "IamIronM@n", "I am the real ironman")
 
 -- Query dos
-SELECT * FROM clients
+SELECT * FROM clients;
 UPDATE clients
 SET clientLevel = 3;
 
 -- Query tres
-SELECT * FROM inventory
+SELECT * FROM inventory;
 UPDATE inventory
 SET invDescription - replace(invDescription, "small interior", "spacious interior")
 WHERE invId = 12
+
+-- Query cuatro
+SELECT * FROM carclassification;
+SELECT * FROM inventory;
+INNER JOIN carclassification ON
+inventory.classificationId = carclassification.classificationId
+WHERE inventory.classificationId = 1;
+
+-- Query cinco
+SELECT * FROM inventory;
+DELETE FROM inventory
+WHERE invId = 1;
+
