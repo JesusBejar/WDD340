a10e2cd54a94221d6e11852af687b1573984aa3c
Jesus Bejar <israelbejarii@gmail.com>
2023-05-08 08:49:41 -0600

    made sql file and added query 1

---
+++ sql/enhancement2.sql
@@ -0,0 +1,4 @@
+-- Query uno
+SELECT * FROM clients;
+INSERT INTO clients (clientEmail, clientFirstname, clientLastname, clientPassword, comment)
+VALUES ("tony@starknet.com", "Tony", "Stark", "IamIronM@n", "I am the real ironman")
