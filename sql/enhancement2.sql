4a4004eac62dcd2377eb85c6924ee55f6c3a12ed
Jesus Bejar <israelbejarii@gmail.com>
2023-05-08 08:52:47 -0600

    made queries 2 &3

--- sql/enhancement2.sql
+++ sql/enhancement2.sql
@@ -2,3 +2,14 @@
 SELECT * FROM clients;
 INSERT INTO clients (clientEmail, clientFirstname, clientLastname, clientPassword, comment)
 VALUES ("tony@starknet.com", "Tony", "Stark", "IamIronM@n", "I am the real ironman")
+
+-- Query dos
+SELECT * FROM clients
+UPDATE clients
+SET clientLevel = 3;
+
+-- Query tres
+SELECT * FROM inventory
+UPDATE inventory
+SET invDescription - replace(invDescription, "small interior", "spacious interior")
+WHERE invId = 12
