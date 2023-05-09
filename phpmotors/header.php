ade642d4bfec979498872fe138531ed493bc0f5a
Jesus Bejar <israelbejarii@gmail.com>
2023-05-09 13:17:00 -0600

    moved phpmotors directly into htdocs

--- phpmotors/header.php
+++
@@ -1,5 +0,0 @@
-<!-- Header -->
-<a href="/template.php" title="Go to the phpmotors.com home page">
-<img src="../images/site/logo.png" alt="php motors logo">
-</a>
-<div id="my-account"> My Account </div>
