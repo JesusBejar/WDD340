bbd4d7cba217f67b93a76845376e3dcc3312285a
Jesus Bejar <israelbejarii@gmail.com>
2023-05-08 20:30:23 -0600

    moved connections.php out of library folder

--- phpmotors/connections.php
+++ phpmotors/connections.php
@@ -9,11 +9,12 @@ function phpmotorsConnect() {
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
      // Create the actual connection object and assign it to a variable
      try {
-        $link = new PDO($dsn, $username, $password, $options);
-        if (is_object($link))
-        {echo "It worked";}
+      $link = new PDO($dsn, $username, $password, $options);
+      if (is_object($link))
+      {echo "It worked";}
        } catch(PDOException $e) {
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD340/phpmotors/serverError.php'; ?> 
+         echo "It didn't work";
+        // <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/WDD340/phpmotors/serverError.php'; ?> 
        }
      }
 phpmotorsConnect()
